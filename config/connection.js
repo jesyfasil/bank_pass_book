9c258a45a36e8da302efe9eb9b53eb32c773c1cf
jaseelafasil <jaseelafasil54@gmail.com>
2021-05-18 10:49:47 -1100

    env variable added

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-//require('dotenv').config()
+require('dotenv').config()
 const Sequelize=require("sequelize")
-const sequelize=new Sequelize("bank_stmt",'root','password',{host:'localhost',dialect:"mysql"})
+const sequelize=new Sequelize("bank_stmt",process.env.DB_USER,DB_PASS,{host:DB_HOST,dialect:"mysql"})
 module.exports={sequelize:sequelize}
